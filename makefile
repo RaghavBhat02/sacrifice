8e04abf5b6b8b79d2cb012a9bcf70cbd1d97707b
Raghav <raghavbhat02@gmail.com>
2021-04-20 16:45:47 -0400

    testing makefile

---
+++ makefile
@@ -0,0 +1,4 @@
+push: 
+	git add . 
+	git commit -m "testing makefile" 
+	git push
--- structs/MessageHandler.js
+++ structs/MessageHandler.js
@@ -13,7 +13,7 @@ module.exports = class MessageHandler {
       if(message.content.toLowerCase().startsWith('jia ru')) {
         message.channel.send("JIA RU!! ALL HAIL TRIBAL LEADER SRONG864!");
         if(message.guild.id != "815617032917745754") return;
-        message.channel.send(process.env.JIARU);
+        message.channel.send(process.env.JIARU); //link to jiaru
       }
     })
 
